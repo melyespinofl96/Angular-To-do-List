340d8dbca04472fb1dd21e7f8bdae4d4b34354cb
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-02-06 22:51:16 -0600

    Added Tailwind CSS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-TODO-List'`, () => {
+  it(`should have as title 'Angular-To-Do-List'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-TODO-List');
+    expect(app.title).toEqual('Angular-To-Do-List');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-TODO-List app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-To-Do-List app is running!');
   });
 });
